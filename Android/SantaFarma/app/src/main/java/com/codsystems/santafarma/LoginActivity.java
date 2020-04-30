8a69c857f05df0bad41f3e1bdff67d3aa233a1a0
gabriedasilva <61169847+gabriedasilva@users.noreply.github.com>
2020-04-30 17:03:43 -0300

    Tela de Cadastro - Cadastrar Usuário

--- Android/SantaFarma/app/src/main/java/com/codsystems/santafarma/LoginActivity.java
+++ Android/SantaFarma/app/src/main/java/com/codsystems/santafarma/LoginActivity.java
@@ -2,7 +2,9 @@ package com.codsystems.santafarma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void abrirTelaCadastro(View view){
+        Intent intent = new Intent(LoginActivity.this, CadastroActivity.class);
+        startActivity(intent);
     }
 }
